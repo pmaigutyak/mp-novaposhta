e55451c939bebad669c3cc23e52d441d6d26af6c
Yevgen Plyusnin <jplyusnin@gmail.com>
2017-03-25 17:24:04 +0200

    Edit admin register.

--- novaposhta/admin.py
+++ novaposhta/admin.py
@@ -4,8 +4,9 @@ from django.contrib import admin
 from novaposhta.models import Warehouse
 
 
-@admin.register(Warehouse)
 class WarehouseAdmin(admin.ModelAdmin):
 
     list_display = ('title', 'address', )
     search_fields = ('title', 'address', )
+
+admin.site.register(Warehouse, WarehouseAdmin)
